5d50aefd8e0829eadf196c1adbda367a2c743d6e
Alexey <classx@gmail.com>
2025-03-08 15:29:22 +0200

    init subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 use tabled::{Table, Tabled};
@@ -40,18 +40,47 @@ struct Repo {
 }
 
 /// Github monitoring tool
+// #[derive(Parser, Debug)]
+// #[command(version, about, long_about = None)]
+// struct Args {
+//     /// Config file path
+//     #[arg(short = 'f', long = "file")]
+//     config: String,
+//     /// Validate config file only
+//     #[arg(short, long, default_value = "false")]
+//     test: bool,
+//     /// Print debug information
+//     #[arg(short = 'd', long = "debug", default_value = "false")]
+//     debug: bool,
+// }
+
+/// CLI tool with subcommands
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Config file path
-    #[arg(short = 'f', long = "file")]
-    config: String,
-    /// Validate config file only
-    #[arg(short, long, default_value = "false")]
-    test: bool,
-    /// Print debug information
-    #[arg(short = 'd', long = "debug", default_value = "false")]
-    debug: bool,
+#[command(
+    name = "Github monitoring tool",
+    version = "1.0",
+    about = "A tool with subcommands"
+)]
+struct Cli {
+    /// Subcommands for specific actions
+    #[command(subcommand)]
+    command: Commands,
+}
+
+/// Define available subcommands
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Add a new item
+    Init {
+        /// Name of the item to add
+        //#[arg(short = 'f', long = "file", default_value = "")]
+        name: String,
+    },
+    /// Remove an item by its ID
+    Remove {
+        /// ID of the item to remove
+        id: u32,
+    },
 }
 
 // struct functions
@@ -91,22 +120,57 @@ fn validation(config: &Config) {
     println!("{table}");
 }
 
-fn main() {
-    let args = Args::parse();
-    let config = load_config(&args.config);
-    if args.test {
-        validation(&config);
-        println!("Config file is valid.");
-        return;
-    }
-    if args.debug {
-        println!("{:?}", config);
-    }
+fn generate_config_yaml(config: &Config) -> std::io::Result<()> {
+    let yaml_str = serde_yaml::to_string(&config).unwrap();
+    std::fs::write("config.yaml", yaml_str)?;
+    Ok(())
+}
 
-    for repo in config.repos {
-        if repo.skip {
-            continue;
+// fn main() {
+//     let args = Args::parse();
+//     let config = load_config(&args.config);
+//     if args.test {
+//         validation(&config);
+//         println!("Config file is valid.");
+//         return;
+//     }
+//     if args.debug {
+//         println!("{:?}", config);
+//     }
+
+//     for repo in config.repos {
+//         if repo.skip {
+//             continue;
+//         }
+//         repo.run();
+//     }
+// }
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Init { name } => {
+            let config = Config {
+                project: name.to_string(),
+                path: "/dir".to_string(),
+                update_frequency_sec: 3600,
+                message: vec![Message {
+                    name: "name".to_string(),
+                    url: "url".to_string(),
+                }],
+                repos: vec![Repo {
+                    name: "repo name".to_string(),
+                    url: "url".to_string(),
+                    branch: "main".to_string(),
+                    skip: false,
+                    silent: false,
+                    files: vec!["file".to_string()],
+                }],
+            };
+            let _ = generate_config_yaml(&config);
         }
-        repo.run();
+        Commands::Remove { id } => {
+            println!("Removing item with ID: {}", id);
+        }
     }
 }
